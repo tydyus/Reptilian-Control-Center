3fff0881fe59606986244d29b4363505fd878298
CheapSeth <cheapseth@electric-dragons.org>
2017-04-22 15:54:56 +0200

    Added main loop, rendering

---
+++ src/rendering.py
@@ -0,0 +1,10 @@
+import sdl2.ext
+
+
+class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
+    def __init__(self, window):
+        super(SoftwareRenderer, self).__init__(window)
+
+    def render(self, components):
+        sdl2.ext.fill(self.surface, sdl2.ext.Color(74, 122, 142))
+        super(SoftwareRenderer, self).render(components)
