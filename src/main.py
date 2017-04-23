627157cd01300669f1d9f762af314556cbd6366b
tydyus@electric-dragons.org <tydyus@electric-dragons.org>
2017-04-23 14:30:35 +0200

    maj input boitier

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from inputBoitier import *
 pygame.init()
 clock = pygame.time.Clock()
 FPS = 60
-vitCycle = 60
+vitCycle = 20
 time = 0
 
 def rot_center(image, angle):
@@ -55,6 +55,10 @@ switch4.etatON = pygame.image.load("../img/switch4ON.png").convert_alpha()
 switch4.etatOFF = pygame.image.load("../img/switch4OFF.png").convert_alpha()
 switch4.img = pygame.image.load("../img/switch4OFF.png").convert_alpha()
 curseurM = pygame.image.load("../img/curseurMoney.png").convert_alpha()
+jaugeB = pygame.image.load("../img/jaugeB.png").convert_alpha()
+alimOn = pygame.image.load("../img/alimon.png").convert_alpha()
+alimOff = pygame.image.load("../img/alimoff.png").convert_alpha()
+        
 
 
 #creation planete/ville
@@ -86,6 +90,7 @@ pygame.display.flip()
 #BOUCLE INFINIE
 
 continuer = 1
+boitierAlim = 0
 cycle = 1
 angle = 0
 mousemove = 0
@@ -102,96 +107,110 @@ while continuer:
     if event.type == MOUSEBUTTONDOWN:
         mousemove = 0
 
-    ## input
+    
+        ## input
     if mousemove == 0 :
-
+        
         #curseur richesse
         if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 497 and event.pos[1] > 202 and event.pos[0] < 590 and event.pos[1] < 275:
             mousemove = 1
             print("repartition de la richesse")
             ville1.repartition, ville2.repartition, ville3.repartition, ville4.repartition, ville5.repartition, ville6.repartition, = rptRich.click()
-        #switch1
+            #switch1
         if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 598 and event.pos[1] > 200 and event.pos[0] < 638 and event.pos[1] < 280:
             mousemove = 1
             print("switch1")
             switch1.click()
-        #switch2
+            #switch2
         if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 650 and event.pos[1] > 200 and event.pos[0] < 692 and event.pos[1] < 280:
             mousemove = 1
             print("switch2")
             switch2.click()
-        #switch3
+            #switch3
         if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 704 and event.pos[1] > 200 and event.pos[0] < 744 and event.pos[1] < 280:
             mousemove = 1
             print("switch3")
             switch3.click()
-        #switch4
+            #switch4
         if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 757 and event.pos[1] > 200 and event.pos[0] < 797 and event.pos[1] < 280:
             mousemove = 1
             print("switch4")
             switch4.click()
-    
-    ## cycle
-    # add habitants
-    if time == vitCycle:
-        print()
-        print("cycle", cycle)
-        time = 0
-        cycle += 1
-        
-        ville1.addPop(1)
-        ville1.checkPop()
-        ville1.addDev()
-        
-        ville2.addPop(2)
-        ville2.checkPop()
-        ville2.addDev()
-        
-        ville3.addPop(3)
-        ville3.checkPop()
-        ville3.addDev()
-        
-        ville4.addPop(4)
-        ville4.checkPop()
-        ville4.addDev()
-        
-        ville5.addPop(5)
-        ville5.checkPop()
-        ville5.addDev()
-        
-        ville6.addPop(6)
-        ville6.checkPop()
-        ville6.addDev()
-        
-    # resolution crise
-    # add dvlp
-    
-    ## visuel départ
-    # visu map
-    fenetre.blit(fond, (0,0))
-    fenetre.blit(world, (140,140)) #imgtaille: 100*100
 
-    city = rot_center(ville1.city, 60)  #ville1
-    fenetre.blit(city, (45,45))
-    city = rot_center(ville2.city, 0)   #ville2
-    fenetre.blit(city, (45,45))
-    city = rot_center(ville3.city, 180) #ville3
-    fenetre.blit(city, (45,45))
-    city = rot_center(ville4.city, 120) #ville4
-    fenetre.blit(city, (45,45))
-    city = rot_center(ville5.city, 300) #ville5
-    fenetre.blit(city, (45,45))
-    city = rot_center(ville6.city, 240) #ville6
-    fenetre.blit(city, (45,45))
+    #Alimentation du boitier
+    if mousemove == 0 and boitierAlim == 0 and event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[0] > 497 and event.pos[1] > 400 and event.pos[0] < 585 and event.pos[1] < 460:
+        switch1.son.play()
+        mousemove = 1
+        boitierAlim = 1
+    if boitierAlim == 1:
+        
+        # maj time
+        time += 1
+        ## cycle
+        # add habitants
+        if time == vitCycle:
+            print()
+            print("cycle", cycle)
+            time = 0
+            cycle += 1
+        
+            ville1.addPop(1)
+            ville1.checkPop()
+            ville1.addDev()
+        
+            ville2.addPop(2)
+            ville2.checkPop()
+            ville2.addDev()
+        
+            ville3.addPop(3)
+            ville3.checkPop()
+            ville3.addDev()
+            
+            ville4.addPop(4)
+            ville4.checkPop()
+            ville4.addDev()
+                
+            ville5.addPop(5)
+            ville5.checkPop()
+            ville5.addDev()
+                
+            ville6.addPop(6)
+            ville6.checkPop()
+            ville6.addDev()
+        
+            # resolution crise
+            # add dvlp
+    
+        ## visuel départ
+        # visu map
+        fenetre.blit(fond, (0,0))
+        fenetre.blit(world, (140,140)) #imgtaille: 100*100
 
-    cloud = rot_center(testR, angle) #test
-    fenetre.blit(cloud, (45,45))
-    angle += 0.4
+        city = rot_center(ville1.city, 60)  #ville1
+        fenetre.blit(city, (45,45))
+        city = rot_center(ville2.city, 0)   #ville2
+        fenetre.blit(city, (45,45))
+        city = rot_center(ville3.city, 180) #ville3
+        fenetre.blit(city, (45,45))
+        city = rot_center(ville4.city, 120) #ville4
+        fenetre.blit(city, (45,45))
+        city = rot_center(ville5.city, 300) #ville5
+        fenetre.blit(city, (45,45))
+        city = rot_center(ville6.city, 240) #ville6
+        fenetre.blit(city, (45,45))
+
+        cloud = rot_center(testR, angle) #test
+        fenetre.blit(cloud, (45,45))
+        angle += 0.4
        
     # visu boitier
     fenetre.blit(boitier, (0,0))
     
     # visu input boitier
+    if boitierAlim == 0:
+        fenetre.blit(alimOff, (0,0))
+    if boitierAlim == 1:
+        fenetre.blit(alimOn, (0,0))
     fenetre.blit(switch1.img, (0,0))
     fenetre.blit(switch2.img, (0,0))
     fenetre.blit(switch3.img, (0,0))
@@ -199,8 +218,13 @@ while continuer:
 
     curseur = rot_center(curseurM, rptRich.angle)
     fenetre.blit(curseur, (497,202))
+
+    # visu graph boitier
+
+    for i in range(ville1.polution()):
+        fenetre.blit(jaugeB, (516,(169-(8*(i-1)))))
+
     
     # Rafraîchissement de l'écran
     pygame.display.flip()
-    # maj time
-    time += 1
+    
--- src/ville.py
+++ src/ville.py
@@ -7,8 +7,11 @@ class villes:
     dvlpCycle = 12 # pre-prod/ de base 12
     dvlpPop = 3 # pre-prod/ de base 3
     
-    dvlp = 0
-    pop = 0
+    dvlp = 0 #developement global
+    pop = 0 #population global
+    rad = 0 #radiation
+    pol = 0 #polution
+    resPol = 0 #resistance polution
     
     def __init__(self):
 
@@ -38,17 +41,36 @@ class villes:
         
         self.dvlp += (villes.dvlpCycle/24*(self.repartition))*(self.valeurDvlp/100)
         print("gagne",(villes.dvlpCycle/24*(self.repartition))*(self.valeurDvlp/100), " a ",self.dvlp, " de dvlp.")
+        villes.pol += (self.dvlp/10)/((100-villes.resPol)/100)
 
         # changement etat de la cité
-        if self.dvlp <= 15:
+        if self.dvlp <= 10:
             self.city = pygame.image.load("../img/city/city_0.png").convert_alpha()
-        if self.dvlp > 15:
+        if self.dvlp > 10:
             self.city = pygame.image.load("../img/city/city_1.png").convert_alpha()
-        if self.dvlp > 50:
+        if self.dvlp > 30:
             self.city = pygame.image.load("../img/city/city_2.png").convert_alpha()
-        if self.dvlp > 200:
+        if self.dvlp > 70:
             self.city = pygame.image.load("../img/city/city_3.png").convert_alpha()
-        if self.dvlp > 500:
+        if self.dvlp > 150:
             self.city = pygame.image.load("../img/city/city_4.png").convert_alpha()
+        if self.dvlp > 225:
+            self.city = pygame.image.load("../img/city/city_5.png").convert_alpha()
+        if self.dvlp > 350:
+            self.city = pygame.image.load("../img/city/city_6.png").convert_alpha()
+        if self.dvlp > 500:
+            self.city = pygame.image.load("../img/city/city_7.png").convert_alpha()
+        if self.dvlp > 700:
+            self.city = pygame.image.load("../img/city/city_8.png").convert_alpha()
+        if self.dvlp > 1000:
+            self.city = pygame.image.load("../img/city/city_9.png").convert_alpha()
+        if self.dvlp > 1400:
+            self.city = pygame.image.load("../img/city/city_10.png").convert_alpha()
         # remise a zero pour le nouveau cycle
         self.valeurDvlp = 100
+
+
+    def polution (self):
+        lvlP = int(villes.pol//500)
+        return lvlP
+            
